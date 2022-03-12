779e5a2f64fe3d71e278890216a720bbbc1af4f8
Brahim HAJJI BABA <brhajji-@e3r12p6.clusters.42paris.fr>
2022-03-12 14:59:56 +0100

    push

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: brhajji- <brhajji-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 17:28:06 by brhajji-          #+#    #+#             */
-/*   Updated: 2022/03/11 18:03:22 by brhajji-         ###   ########.fr       */
+/*   Updated: 2022/03/11 23:36:46 by brhajji-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,26 @@
 # include <stdlib.h>
 # include <pthread.h>
 # include <sys/time.h>
+# include <time.h>
+
+typedef struct s_philo
+{
+	pthread_t		*philo;
+	struct s_philo	*next;
+	int				status;
+	struct timeval	time;
+}	t_philo;
+
+typedef struct s_utils
+{
+	pthread_mutex_t	*fchette;
+	struct s_philo	*philos;
+	int				nb_philo;
+	struct timeval	tteat;
+	struct timeval	ttdie;
+	struct timeval	ttsleep;
+	int				rot;
+}	t_utils;
 
 int	ft_atoi(const char *str);
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,42 +6,102 @@
 /*   By: brhajji- <brhajji-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 17:27:33 by brhajji-          #+#    #+#             */
-/*   Updated: 2022/03/11 18:04:25 by brhajji-         ###   ########.fr       */
+/*   Updated: 2022/03/11 23:47:30 by brhajji-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-static int	table()
+void	eat(t_utils *utils, t_philo *philo)
+{
+	usleep(utils->tteat.tv_usec * 1000);
+}
+
+void	s_sleep(t_utils *utils, t_philo *philo)
+{
+	usleep(utils->ttsleep.tv_usec  * 1000);
+}
+
+void	take_fork(t_philo *philo)
 {
 
 }
 
+void	give_fork(t_philo *philo)
+{
+
+}
+
+void	die(t_philo *philo)
+{
+	gettimeofday(&(philo->time), NULL);
+	philo->status = 0;
+}
+
+static int table(t_utils *utils, t_philo *philo)
+{
+	int	x;
+	struct timeval	tmp;
+
+	x = utils->rot;
+	gettimeofday(&(tmp), NULL);
+	if (x < 0)
+		x = 1;
+	while (x)
+	{
+		if (utils->philos->time.tv_sec - tmp.tv_usec == utils->ttdie.tv_usec)
+			die(philo);
+		if (utils->rot)
+			x--;
+	}
+}
+
+void	init(t_utils *utils, char **av)
+{
+	int	i;
+	t_philo	*tmp;
+
+	i = -1;
+	utils = malloc(sizeof(t_utils));
+	utils->nb_philo = ft_atoi(av[1]);
+	utils->philos = malloc(sizeof(t_philo));
+	utils->fchette = malloc(sizeof(pthread_mutex_t) * (utils->nb_philo));
+	while (++i < utils->nb_philo)
+	{
+		tmp = malloc(sizeof(t_philo));
+		gettimeofday(&(tmp->time), NULL);
+		tmp->next = utils->philos;
+		if (!utils->philos)
+		{
+			utils->philos = tmp;
+			utils->philos->next = NULL;
+		}
+	}
+	
+}
+
 int	main(int ac, char **av)
 {
-	pthread_mutex_t	*fchette;
-	pthread_t		*philo;
+	t_utils			*utils;
 	struct timeval	*tv;
-	int				nb_philo;
 	int				i;
 	int				ret;
 
+	(void)(ac);
+	(void)(av);
+	init(utils, av);
+	/*
 	if (ac == 5 || ac == 6)
 	{
 		i = 0;
-		nb_philo = ft_atoi(av[1]);
-		fchette = malloc(sizeof(pthread_mutex_t) * (nb_philo));
-		philo = malloc(sizeof(pthread_t) * (nb_philo));
-		while (i < philo)
+		while (i < utils->nb_philo)
 		{
-			ret = pthread_create (&philo[i], NULL, table, NULL);
+			/*ret = pthread_create (utils->philos->philo[i], NULL, table, utils);
 			if (!ret)
 			{
-				
-			}
 		}
-		
 	}
 	else
 		printf("Invalid args");
+			}*/
 }
